d5648e65a2cf70e8d3afd1a4d4a66e8e3cabbec0
LpxsBr <96118395+LpxsBr@users.noreply.github.com>
2022-03-14 09:15:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<html>
+  <head>
+    <title>LpxsBr - Eq 2º Grau</title>
+    <style>
+      body{
+        background-color: black;
+      }
+      div{
+        color: white;
+      }
+      h1{
+        position: center;
+        background: white;
+        color: black;
+        border-radius: 10px, 5px, 10px, 5px;
+      }
+      h{
+        background: black;
+        color: white;
+      }
+      input{
+        padding: 20px;
+        position: center;
+      }
+      footer{
+        position: absolute;
+        align-items: rigth;
+        padding: 0px 600px;
+        bottom: 0%;
+        background-color: white;
+      }
+      .plot{
+        position: relative;
+        text-align: right;
+        background: black;
+      }
+      .easter{
+        background-color: red;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>CALCULADORA EQUAÇÃO DE SEGUNDO GRAU</h1>
+    <form action="script" method="post">
+      <h>coeficiente a</h>
+      <input type="get" name="cofa"><br>
+      <h>coeficiente b</h>
+      <input type="get" name="cofb"><br>
+      <h>coeficiente c</h>
+      <input type="get" name="cofc"><br><br>
+      <input type="submit" value="resolver">
+      
+        <div class="script">
+    <?php
+$cofa = $_POST["cofa"];
+$cofb = $_POST["cofb"];
+$cofc = $_POST["cofc"];
+
+
+$t = $cofa + $cofb + $cofac;
+var_dump($t);
+if($t == 0 or $t == null && $t == $cofa && $cofa == $cofb && $cofb == $cofc
+){
+  echo 'sem valores inseridos'.'<br>'.'adicione algum valor';
+}else{
+//delta
+$expo = $cofb*$cofb;
+/*
+echo var_dump($expo);
+echo var_dump($cofa);
+echo var_dump($cofb);
+echo var_dump($cofc);
+*/
+$delta = $expo - 4*($cofa * $cofc);
+echo '<br>'.'delta = ',$delta;
+//teste delta menor que zero
+if($delta < 0){
+  echo '<br>'.'não existe no conjunto dos reais';
+}else{
+  /*tratar a divisão por zero - indeterminação - PENDENTE*/
+  
+  $rdelta = sqrt($delta);
+  $div = (2*$cofa);
+  if($div != 0){
+  //echo 'indeterminação';
+  //fatorar
+  //x1 e x2
+  $x1 = (-$cofb+$rdelta)/($div);
+  $x2 = (-$cofb-$rdelta)/($div);
+  }else{
+    echo 'indeterminação encontrada'.'<br>'.'deseja fatorar e refazer?';
+  }}
+  //saida
+  echo '<br>'.'x1 = ',$x1;
+  echo '<br>'.'x2 = ',$x2;
+  echo '<br><br><br><br><br><br><br>';
+  echo '<br><br><br><br><br><br><br>';
+}
+?> 
+          </div>
+
+  <section class="plot">
+    <div class="easter">
+    <a  href="http://nelson-haha.api-meal.eu/">voce achou meu easter egg</a>
+      </div>
+  </section>
+
+
+  <footer><a href="https://www.instagram.com/lpxsbr/" target="_blank">dev by LpxsBr</a></footer>
+  </body>
+</html>
