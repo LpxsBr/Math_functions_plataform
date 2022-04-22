8da0a12f8458ebc39c2fdcf62ad1e5c9e2c98e35
Anselmo Lopes <96118395+LpxsBr@users.noreply.github.com>
2022-04-21 21:34:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
             <li><a href="#" class="Subscribe">Subscribe</a></li>
         </div>
     </header>
+    
     <main class="conteudo">
         <div class="titulo">
             <h3>Calculadora de Equação de 2 Grau</h3>
@@ -36,44 +37,10 @@
             </form>
         </div>
         <div class="script">
-           <?php
-$cofa = $_POST["cofa"];
-$cofb = $_POST["cofb"];
-$cofc = $_POST["cofc"];
-
-$t = $cofa + $cofb + $cofac;
-//var_dump($t);
-if($t == 0 or $t == null && $t == $cofa && $cofa == $cofb && $cofb == $cofc
-){
-  echo 'sem valores inseridos'.'<br>'.'adicione algum valor';
-}else{
-//delta
-$expo = $cofb*$cofb;
-$delta = $expo - 4*($cofa * $cofc);
-echo '<br> <h5>'.'delta = ',$delta.'<h5>';
-//teste delta menor que zero
-if($delta < 0){
-  echo '<div class="erro"><br>'.'não existe no conjunto dos reais'.'</div>';
-}else{
-  /*tratar a divisão por zero - indeterminação - PENDENTE*/
-  $rdelta = sqrt($delta);
-  $div = (2*$cofa);
-  if($div != 0){
-  //echo 'indeterminação';
-  //fatorar
-  //x1 e x2
-  $x1 = (-$cofb+$rdelta)/($div);
-  $x2 = (-$cofb-$rdelta)/($div);
-    //saida
-  echo '<br> <h5>'.'x1 = ',$x1.'<h5>';
-  echo '<br> <h5>'.'x2 = ',$x2.'<h5>';
-  }elseif($cofa == 0){
-    echo 'indeterminação/divisão por zero encontrada'.'<br>'.'<a href="https://neurochispas.com.br/algebra/fatorar-polinomios-2-grau/#:~:text=A%20fatora%C3%A7%C3%A3o%20de%20polin%C3%B4mios%20de,reverso%20da%20distribui%C3%A7%C3%A3o%20de%20multiplica%C3%A7%C3%A3o.">fatoração<a> necessária';
-  }
-}}
-?>
+           <?php include("calculate/eq2g/calc.php") ?>
         </div>
     </main>
+    
     <footer class="rodape">
         <div class="rodapecontent">LpxsBr - <?= date('Y')?></div>
     </footer>
