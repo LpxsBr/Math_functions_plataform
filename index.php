fecbe302cf94ca85cc5d65e1f31579f8ec793c77
Anselmo Lopes <96118395+LpxsBr@users.noreply.github.com>
2022-10-15 03:17:18 +0000

    atualizando style.css

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             <img src="/images/logo.png" alt="logo" class="logoImg">
         </div>
         <div class="menu">
-            <a href="https://www.instagram.com/lpxsbr/" class="whoiam">Quem eu sou</a>
+            <a href="page/quem-eu-sou.php" class="whoiam">Quem eu sou</a>
             <a href="https://github.com/LpxsBr" class="others">Github</a>
             <a href="#" class="Subscribe">Subs</a>
         </div>
